4d4531262836d95ed8093489b0df63df94fb721d
Ton Nguyen <104323824+tonnguyen123@users.noreply.github.com>
2025-06-07 23:26:40 -0500

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,7 +19,7 @@ export const Login = () => {
         console.log("Sending login:", UserLogin);
     
         try {
-            const response = await fetch ('http://localhost:5041/api/login',{
+            const response = await fetch ('https://full-stack-project-tracker.onrender.com/api/login',{
                 method: 'POST',
                 headers:{
                     'Content-Type': 'application/json',
